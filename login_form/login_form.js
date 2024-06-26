c0cfe0616c33730f0aafcc34df69dcf4bdf9ac7a
JustinLe <nghiale1004@gmail.com>
2024-06-26 22:02:28 +0700

    day 10 exercise

--- login_form/login_form.js
+++ login_form/login_form.js
@@ -1,14 +1,24 @@
 const form = document.getElementById('login_form');
 
+function myGreeting(username) {
+  let message = "Welcome to NonTechLearning class".concat(' ', username)
+
+  alert(message)
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
   
-    let username = document.getElementById("username");
-    let password = document.getElementById("password");
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
   
-    if (username.value == "" || password.value == "") {
-      alert("Username or password is empty")
+    if (username.value == "") {
+      document.getElementById("form_missing_username").removeAttribute("hidden")
+    } else if (password.value == "") {
+      document.getElementById("form_missing_password").removeAttribute("hidden")
     } else {
-      document.getElementById("form_submit_success").innerHTML = '<div>Success get the form<div>'
+      document.getElementById("form_submit_success").removeAttribute("hidden")
+
+      setTimeout(myGreeting(username), 5000);
     }
   });
