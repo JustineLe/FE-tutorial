577b43f547c576aae85e61e055c4a0ae9ca22bb8
JustineLe <nghiale1004@gmail.com>
2024-06-27 20:19:45 +0700

    FE tutorial day 11

--- login_form/login_form.js
+++ login_form/login_form.js
@@ -9,16 +9,26 @@ function myGreeting(username) {
 form.addEventListener("submit", (e) => {
     e.preventDefault();
   
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
   
     if (username.value == "") {
       document.getElementById("form_missing_username").removeAttribute("hidden")
+
+      let form_missing_item = document.getElementById("form_missing_password")
+      if (!(form_missing_item.hasAttribute('hidden'))) {
+        form_missing_item.setAttribute("hidden")
+      }
+
+      let form_success_item = document.getElementById("form_submit_success")
+      if (!(form_success_item.hasAttribute('hidden'))) {
+        form_success_item.setAttribute("hidden")
+      }
     } else if (password.value == "") {
       document.getElementById("form_missing_password").removeAttribute("hidden")
     } else {
       document.getElementById("form_submit_success").removeAttribute("hidden")
 
-      setTimeout(myGreeting(username), 5000);
+      setTimeout(myGreeting(username.value), 5000);
     }
   });
---
+++ todo_form/todo_form.js
@@ -0,0 +1,19 @@
+let todoList = ['Do the exercise']
+
+// function renderTodoList() {
+//   document.getElementById('todo-list') = ''
+//   todoList.forEach((ele) => {
+//     document.getElementById('todo-list') += '<li>' + ele + '</li>'
+//   })
+// }
+
+function onAddTodoItem() {
+  let todoItemValue = document.getElementById('input-todo');
+  // todoList.push(todoItemValue.value);
+  console.log(todoItemValue.value);
+
+  // document.getElementById('input-todo') = '';
+  // renderTodoList();
+
+  // localStorage.
+}
